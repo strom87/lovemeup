68c5a1aa95db0595c0115bfd094fc8e306b7a7b3
strom87 <daniel.strom.87@gmail.com>
2014-05-09 22:57:00 +0200

    added UserRelation model

--- site/app/models/User.php
+++ site/app/models/User.php
@@ -12,6 +12,8 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $table = 'users';
 
+	protected $fillable = array('name', 'email', 'password', 'birthYear', 'length', 'token');
+
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
---
+++ site/app/models/UserRelation.php
@@ -0,0 +1,9 @@
+<?php
+
+class UserRelation extends Eloquent {
+
+	protected $table = 'user_relations';
+
+	protected $fillable = array('minage', 'maxage');
+	
+}
